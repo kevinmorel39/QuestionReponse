273565d4beb22f84d7dfe770eab92c7cb327797a
PavlikKi <killian.pavlik@gmail.com>
2018-02-15 14:08:59 +0100

    creation classes + layout + declaration dans manifest

---
+++ app/src/main/java/fr/eni/android/questionreponse/StatistiquesActivity.java
@@ -0,0 +1,19 @@
+package fr.eni.android.questionreponse;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by PavlikKi on 15/02/2018.
+ */
+
+public class StatistiquesActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistiques);
+    }
+
+
+}
